b23e162b634b465f0acab6abe7dbb7bd8702d628
Del <del.larsen@gmail.com>
2018-01-18 13:58:39 -0600

    Push with gitigrnore

--- checkerboard.js
+++ checkerboard.js
@@ -1,32 +1,40 @@
 var totalRows = prompt("How many Rows do you want?");
 
-
 function CheckerBoard (props) {
     let arrCheckers = [];
-    console.log("Checker prop:" +props)
+    console.log("Checker Props")
+    console.log(props)
     for (let i = 0; i < props.num; i++){
             let boardKey = "b"+i;
-            arrCheckers.push(React.createElement(Row, {num:props.num, key:boardKey}))
-        }
-    
-
+            console.log("Board Key:" )
+            console.log(boardKey)
+                arrCheckers.push(React.createElement(Row, {num:props.num, key:boardKey, superKey:i   }))
+               // arrCheckers.push(React.createElement(Cell, {color:'red', boardKey}))
+            
+            }
+        
     return React.createElement('div',null, arrCheckers)
   }
+
   function Row (props) {
-      console.log("Row:" +props)
+      console.log("props superKey:")
       let arrCell = [];
       for (let i = 0; i < props.num; i++){
-          if (i % 2 === 0) {
-              arrCell.push(React.createElement(Cell, {color:'red', key:i}))
-          } else {
+          console.log("cellKey: " + i)
+          console.log(props.superKey+i)
+          const IsRed = (props.superKey + i) % 2 === 0
+          if (IsRed) {
               arrCell.push(React.createElement(Cell, {color:'black', key:i}))
+            } else {
+                arrCell.push(React.createElement(Cell, {color:'red', key:i}))
           }
     }
-    return React.createElement('div', null, arrCell)
+    return React.createElement('div', {style: {...styles.row}}, arrCell)
   }
-  function Cell (props) {
-    return React.createElement('div', {style: {...styles.cell, ...styles.colorA, backgroundColor:props.color,} }, '')
 
+  
+  function Cell (props) {
+    return React.createElement('div', {style: {...styles.cell, backgroundColor:props.color,} }, '')
   }
   
   
@@ -42,4 +50,5 @@ ReactDOM.render(
     //    React.createElement(Cell, {color:'red'}),
         React.createElement(CheckerBoard,{num:totalRows}),
         document.getElementById('root')
+  
 )
